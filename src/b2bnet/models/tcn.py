7ade3f9aa5f3914c198136c2327792b238f69286
Morteza Ansarinia <ansarinia@me.com>
2023-04-01 12:24:04 +0200

    add stubs for TCN

---
+++ src/b2bnet/models/tcn.py
@@ -0,0 +1,9 @@
+import torch
+from torch import nn
+
+class TCN(nn.Module):
+    def __init__(self):
+        super(TCN, self).__init__()
+
+    def forward(self, x):
+        pass
---
+++ src/b2bnet/models/tcn_block.py
@@ -0,0 +1,9 @@
+import torch
+from torch import nn
+
+class TCNBlock(nn.Module):
+    def __init__(self):
+        super(TCNBlock, self).__init__()
+
+    def forward(self, x):
+        pass
