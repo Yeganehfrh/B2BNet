83c16329219913f1a6a7fde7f80315b49a145ed6
Yeganeh Farahzadi <36996819+Yeganehfrh@users.noreply.github.com>
2023-08-14 12:02:55 +0200

    organize folders

--- src/b2bnet/data/utils.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-
-def padding(x, pad_length=64):
-    return torch.nn.functional.pad(x,
-                                   pad=(0, 0, pad_length, 0),
-                                   mode='constant',
-                                   value=0)
