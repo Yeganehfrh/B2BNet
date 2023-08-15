573bdd3f3e680bdebbd4856a2cfbee7532160a41
Morteza Ansarinia <ansarinia@me.com>
2023-08-15 22:08:51 +0200

    refactor notebooks

--- src/b2bnet/__init__.py
+++ src/b2bnet/__init__.py
@@ -1,11 +1,11 @@
-from .models.old.b2bnet_model import B2BNetModel
-from .models.tcn import TCN
+from .models.archived.b2bnet_model import B2BNetModel
+from .models.archived.tcn import TCN
 from .data.random_data import RandomDataModule
 from .data.otka_subject_split import OtkaDataModule
 from .data.otka_time_split import OtkaTimeDimSplit
-from .models.b2bnet_spacetime_model import B2BNetSpaceTimeModel  # noqa
-from .models.bidirectional_lstm import BidirectionalLSTM  # noqa
-from .utils.timeseries import lag, padding  # noqa
+from .models.archived.b2bnet_spacetime_model import B2BNetSpaceTimeModel  # noqa
+from .models.archived.bidirectional_lstm import BidirectionalLSTM  # noqa
+from .utils.timeseries import lag, pad  # noqa
 
 __all__ = [
     'B2BNetModel',
