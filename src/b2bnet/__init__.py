c75ecd27dd83d60d7e643bd5a3c4a27213da06ff
Morteza Ansarinia <ansarinia@me.com>
2023-08-15 13:47:36 +0200

    cleanups

--- src/b2bnet/__init__.py
+++ src/b2bnet/__init__.py
@@ -1,8 +1,8 @@
 from .models.b2bnet_model import B2BNetModel
 from .models.tcn import TCN
 from .data.random_data import RandomDataModule
-from .data.otka_data import OtkaDataModule
-from .data.otka_data_time import OtkaTimeDimSplit
+from .data.otka_subject_split import OtkaDataModule
+from .data.otka_time_split import OtkaTimeDimSplit
 from .models.b2bnet_spacetime_model import B2BNetSpaceTimeModel  # noqa
 from .models.bidirectional_lstm import BidirectionalLSTM  # noqa
 from .utils.timeseries import lag, padding  # noqa
