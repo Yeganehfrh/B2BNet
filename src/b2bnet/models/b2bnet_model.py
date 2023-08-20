9660076bddd1f5b49fdd541b3479662ffbe91da1
Yeganeh Farahzadi <36996819+Yeganehfrh@users.noreply.github.com>
2023-08-20 12:45:29 +0200

    change namings + recalculate loss function differently + save hyperparameters

--- src/b2bnet/models/b2bnet_model.py
+++ src/b2bnet/models/b2bnet_model.py
@@ -12,23 +12,24 @@ class B2BNetSpaceTimeModel(pl.LightningModule):
     def __init__(self,
                  n_channels, space_embedding_dim, time_embedding_dim,
                  n_subjects, kernel_size=1,
-                 base: Literal['autoencoder', 'encoder'] = 'encoder',
-                 b2b: Literal['decoder', 'embedding'] | None = None):
+                 encoder_arch: Literal['autoencoder', 'encoder_only'] = 'encoder_only',
+                 b2b: Literal['decoder', 'embedding'] | None = None,
+                 classifier: bool = True):
         super().__init__()
+        self.save_hyperparameters()
         self.b2b = b2b
-        self.base = base
+        self.encoder_arch = encoder_arch
+        self.classifier = classifier
+        self.n_subjects = n_subjects
 
         # baseline
-        if self.base == 'autoencoder':
-            self.n_subjects = n_subjects
-            self.b2b = b2b
-
-            self.base = SpaceTimeAutoEncoder(
+        if self.encoder_arch == 'autoencoder':
+            self.baseline = SpaceTimeAutoEncoder(
                 n_channels, space_embedding_dim, time_embedding_dim,
                 kernel_size=kernel_size)
 
-        if self.base == 'encoder':
-            self.base = SpaceTimeEncoder(
+        if self.encoder_arch == 'encoder_only':
+            self.baseline = SpaceTimeEncoder(
                 n_channels, space_embedding_dim, time_embedding_dim,
                 kernel_size=kernel_size)
 
@@ -41,16 +42,17 @@ class B2BNetSpaceTimeModel(pl.LightningModule):
                                              kernel_size=kernel_size)
 
         # classifier head
-        self.cls = nn.Linear(time_embedding_dim, 2)
+        if self.classifier:
+            self.cls = nn.Linear(time_embedding_dim, 2)
 
     def forward(self, x, x_b2b):
 
-        # base
-        embedding, y_hat = self.base(x)
+        # baseline
+        embedding, y_hat = self.baseline(x)
+
+        y_b2b_hat = embedding_b2b = y_cls = None  # default values
 
         # b2b
-        y_b2b_hat = embedding_b2b = None  # default values for b2b head
-
         if self.b2b == 'decoder':
             y_b2b_hat = self.b2b_head(embedding, n_timesteps=x.shape[1])
 
@@ -58,29 +60,30 @@ class B2BNetSpaceTimeModel(pl.LightningModule):
             embedding_b2b, _ = self.b2b_head(x_b2b)
 
         # classifier
-        y_cls = self.cls(embedding[-1, :, :])
+        if self.classifier:
+            y_cls = self.cls(embedding[-1, :, :])
 
-        return embedding, y_hat, y_b2b_hat, embedding_b2b, y_cls
+        return embedding, y_hat[:, -1, :], y_b2b_hat, embedding_b2b, y_cls
 
     def training_step(self, batch, batch_idx):
         x, y, subject_ids, x_b2b, y_b2b, y_cls = batch
         embedding, y_hat, y_b2b_hat, embedding_b2b, y_cls_hat = self(x, x_b2b)
+        
+        loss_reconn = nn.functional.mse_loss(y_hat, y)
+        loss = loss_reconn
+        self.log('train/loss_reconn', loss_reconn)
 
         # loss & logging
-        loss_cls = nn.functional.cross_entropy(y_cls_hat, y_cls)
-        self.log('train/loss_cls', loss_cls)
-        loss = loss_cls
+        if self.classifier:
+            loss_cls = nn.functional.cross_entropy(y_cls_hat, y_cls)
+            self.log('train/loss_cls', loss_cls)
+            loss += loss_cls
 
-        accuracy = tmf.accuracy(y_cls_hat, y_cls, task='multiclass', num_classes=2)
-        self.log('train/accuracy', accuracy)
-
-        if self.base == 'autoencoder':
-            loss_reconn = nn.functional.mse_loss(y_hat, y)
-            loss += loss_reconn
-            self.log('train/loss_reconn', loss_reconn)
+            accuracy = tmf.accuracy(y_cls_hat, y_cls, task='multiclass', num_classes=2)
+            self.log('train/accuracy', accuracy)
 
         if self.b2b == 'decoder':
-            loss_b2b = nn.functional.mse_loss(y_b2b_hat, y_b2b)
+            loss_b2b = nn.functional.mse_loss(y_b2b_hat[:, -1, :], y_b2b)
             loss += loss_b2b
             self.log('train/loss_b2b', loss_b2b)
 
@@ -96,20 +99,20 @@ class B2BNetSpaceTimeModel(pl.LightningModule):
         x, y, subject_ids, x_b2b, y_b2b, y_cls = batch
         embedding, y_hat, y_b2b_hat, embedding_b2b, y_cls_hat = self(x, x_b2b)
 
-        # losses & logging
-        loss_cls = nn.functional.cross_entropy(y_cls_hat, y_cls)
-        self.log('val/loss_cls', loss_cls)
-        loss = loss_cls
-        accuracy = tmf.accuracy(y_cls_hat, y_cls, task='multiclass', num_classes=2)
-        self.log('val/accuracy', accuracy)
+        loss_reconn = nn.functional.mse_loss(y_hat, y)
+        loss = loss_reconn
+        self.log('val/loss_reconn', loss_reconn)
 
-        if self.base == 'autoencoder':
-            loss_reconn = nn.functional.mse_loss(y_hat, y)
-            loss += loss_reconn
-            self.log('val/loss_reconn', loss_reconn)
+        # losses & logging
+        if self.classifier:
+            loss_cls = nn.functional.cross_entropy(y_cls_hat, y_cls)
+            self.log('val/loss_cls', loss_cls)
+            loss += loss_cls
+            accuracy = tmf.accuracy(y_cls_hat, y_cls, task='multiclass', num_classes=2)
+            self.log('val/accuracy', accuracy)
 
         if self.b2b == 'decoder':
-            loss_b2b = nn.functional.mse_loss(y_b2b_hat, y_b2b)
+            loss_b2b = nn.functional.mse_loss(y_b2b_hat[:, -1, :], y_b2b)
             loss += loss_b2b
             self.log('val/loss_b2b', loss_b2b)
 
